cb1c35b3339fb0e10e101f9e21e3a1c5c619ce0d
Assad Isah <nottherealalanturing@gmail.com>
2022-08-13 01:04:56 +0100

    fix: resolve errors with rubocop

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,26 +1,25 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get home" do
+  test 'should get home' do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select 'title', "Home | #{@base_title}"
   end
-  
-  test "should get help" do
+
+  test 'should get help' do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select 'title', "Help | #{@base_title}"
   end
-  
-  test "should get about" do
+
+  test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select 'title', "About | #{@base_title}"
   end
 end
