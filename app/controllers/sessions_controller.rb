ed8890788779a493c1478519642518108f22d73f
Assad Isah <nottherealalanturing@gmail.com>
2022-08-22 08:05:27 +0100

    updates to rails 7

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #log the user in
+      log_in user
+      redirect_to user
     else
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
 end
