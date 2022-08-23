34a8138ca2b56e0dea14fc4e473b6881dfa81026
Assad Isah <nottherealalanturing@gmail.com>
2022-08-23 14:33:10 +0100

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,14 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    log_out
+    redirect_to root_url, status: :see_other
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
       log_in user
       redirect_to user
     else
