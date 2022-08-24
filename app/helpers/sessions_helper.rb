a817fee12d42a70934315aacacc0585671ef7b03
Assad Isah <nottherealalanturing@gmail.com>
2022-08-25 00:19:24 +0100

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,10 +4,22 @@ module SessionsHelper
         session[:user_id] = user.id
     end
 
+    def remember(user)
+        user.remember
+        cookies.permanent.encrypted[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+
     # Returns the current logged-in user (if any).
     def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.encrypted[:user_id])
+            user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
         end
     end
 
@@ -18,6 +30,13 @@ module SessionsHelper
 
     def log_out
         reset_session
+        @current_user = nil
+    end
+
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
         @current_user = nil
     end
 end
