703adccaa6dee0b7d7cf5269bcb8bbe187af8dac
Assad Isah <nottherealalanturing@gmail.com>
2022-08-28 12:30:02 +0100

    add feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_121812) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_211305) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_121812) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,17 @@ User.create!(name: "Example User",
     email: "example@railstutorial.org",
     password: "foobar",
     password_confirmation: "foobar",
-    admin: true)
+    admin: true,
+    activated: true,
+    activated_at: Time.zone.now)
 
 User.create!(name: "Assad Isah",
         email: "nottherealalanturing@gmail.com",
         password: "foobar",
         password_confirmation: "foobar",
-        admin: true)
+        admin: true,
+        activated: true,
+        activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -19,5 +23,7 @@ User.create!(name: "Assad Isah",
     User.create!(name: name,
         email: email,
         password: password,
-        password_confirmation: password)
+        password_confirmation: password,
+        activated: true,
+        activated_at: Time.zone.now)
 end
